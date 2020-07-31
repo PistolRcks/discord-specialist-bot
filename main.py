a5127eba1e5f3ec66b7845d2f659819e41b7d8f4
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2020-07-30 19:44:05 -0500

    Added specialist command and renamed  to

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 from discord.errors import LoginFailure
 from PIL import Image
-import specialist
+import rendering
 
 bot = commands.Bot(command_prefix="!")
 
@@ -20,11 +20,13 @@ async def on_message(message):
 
 @bot.listen()
 async def on_command_error(ctx, error):
-	if type(error) == commands.errors.ExpectedClosingQuoteError:
-		await ctx.send(f"nice job with the quotation marks, {ctx.author.mention}")
-	else:
-		print(f"Error {type(error)} pushed!")
-	
+    if type(error) == commands.errors.ExpectedClosingQuoteError:
+        await ctx.send(f"nice job with the quotation marks, {ctx.author.mention}")
+    elif type(error) == commands.errors.MissingRequiredArgument:
+        await ctx.send(f"you're missing some command arguments, {ctx.author.mention}")
+    else:
+        print(f"Error `{error}` of type `{type(error)}` pushed!")
+
 
 # Just a test command
 @bot.command()
@@ -35,9 +37,9 @@ async def smd(ctx):
 @bot.command()
 async def impact(ctx, topText, bottomText):
 	# Make the image
-	rawImage = specialist.createTextOverlay(topText, bottomText, fontSize=25)
+	rawImage = rendering.createTextOverlay(topText, bottomText, fontSize=25)
 	image = Image.frombytes("RGBA", specialist.SIZE, rawImage)
-	
+
 	# Save the image
 	print("Saving image...")
 	try:
@@ -47,7 +49,34 @@ async def impact(ctx, topText, bottomText):
 		os.remove("img-tmp.png")
 		print("Removed temporary file.")
 	except OSError:
-		print("Image failed to save, so not sent.") 
+		print("Image failed to save, so not sent.")
+
+# Creates the specialist meme and then sends it
+@bot.command()
+async def specialist(ctx, topText, bottomText):
+    # Make sure it exists before screwing around
+    if not os.path.exists("specialist.mp4"):
+        print("Specialist not found! Make sure it's named `specialist.mp4`.")
+        pass
+    # Cleanup if command failed last time
+    try: os.remove("specialist-tmp.mp4")
+    except: pass
+
+    # Render it
+    result = rendering.renderWithTextOverlay("specialist.mp4", "specialist-tmp.mp4", \
+        rendering.createTextOverlay(topText, bottomText), \
+        14, 25)
+
+    # Send it
+    if result == 0:
+        await ctx.send(file=discord.File("specialist-tmp.mp4", filename=f"funny_specialist_meme_from_{ctx.author}.mp4"))
+    else:
+        print("Render failed, so not sending.")
+        await ctx.send("Render failed. Please try again.")
+
+    # Cleanup
+    try: os.remove("specialist-tmp.mp4")
+    except OSError: print("Cleanup failed or temporary file did not exist in the first place.")
 
 
 try: bot.run(sys.argv[1]) # Start it UP
